9c054ef8dee44db7c735509b56b02a6fc5093503
Giorgi <115803488+Kochli@users.noreply.github.com>
2026-06-28 23:54:41 +0400

    chore: remove unused conversation-tags module

--- src/shared/conversation-tags/index.ts
+++
@@ -1,5 +0,0 @@
-export { useConversationTags } from './useConversationTags';
-export type { ConversationTagInput, ConversationTagsBundle } from './useConversationTags';
-export { INBOX_TAG_LABEL } from './types';
-export type { InboxTag, ConversationTagResult } from './types';
-export { normaliseHandle, emailDomain } from './normalise';
